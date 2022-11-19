23462592e27d2bc34cc1e7fea9726cab04904b4d
Radha02062001 <rajalakshmi96462@gmail.com>
2022-11-19 18:27:31 +0530

    update

---
+++ src/components/web/web.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./web.css"
+
+const Web =()=>{
+    return(
+         
+
+    <div className="web">
+        <h1>Reshaping Dynamics of Modern-Age<br/>
+Businesses With Our Solutions</h1>
+       <h2>Your doorway of assured growth through the commitment of qualitative delivery</h2>
+       <div className="mobile">
+        <div className="mob1 ">
+            <div className="back">
+                <div className="overlay">
+                    <h4><button></button>Web Design <br/><br/><button></button>eCommerce Website Design
+                    <br/><br/><button></button>Logo Design<br/><br/><button></button>Corporate Website Design
+                    <br/><br/><button></button>B2B Website Design
+                    </h4>
+ 
+ </div></div>
+            <h2>Captivating Design <br/>Services</h2>  
+            <h4>A good design is the first good impression<br/> you would make on your customers.</h4>   
+        </div>
+        <div className="mob2 "><h2>Web and Mobile App <br/>Development</h2>
+        <p>Begin your journey to create revenue-generating<br/> digital solutions for your business.</p>
+        </div>
+        <div className="mob3 "> 
+        <div className="over">
+                    <h4><button></button>Android App Development<br/><br/><button></button>iOS App Development
+                    <br/><br/><button></button>Hybrid App Development<br/><br/><button></button>
+                    React Native App Development<br/><br/><button></button>Flutter App Development
+                    <br/><br/><button></button>Web Development<br/><br/><button></button>Java Development
+                    <br/><br/><button></button>.NET Development<br/><br/><button></button>Python Development
+                    <br/><br/><button></button>PHP Development
+                    </h4>
+ </div>
+  </div></div>
+  <div className="lay">
+        <div className="mob4 ">
+        <div className="overlay">
+                    <h4><button></button>Blockchain Development<br/><br/><button></button>Enterprise Blockchain Development
+                    <br/><br/><button></button>Cryptocurrency Wallet Development<br/><br/><button></button>
+                    NFT Marketplace Development
+                    <br/><br/><button></button>Smart Contracts Development
+                    </h4>
+ 
+ </div>
+        </div>
+        <div className="mob5 ">
+        <div className="overlay">
+                    <h4><button></button>Salesforce<br/><br/><button></button>AWS
+                    <br/><br/><button></button>Azure<br/><br/><button></button>
+                    Google Cloud
+                    </h4>
+ 
+ </div>
+        </div>
+        <div className="mob6 ">
+        <div className="overlay">
+                    <h4><button></button>WooCommerce Development<br/><br/><button></button>Magento Development
+                    <br/><br/><button></button>eCommerce App Development<br/><br/><button></button>B2B eCommerce
+                    <br/><br/><button></button>WordPress Development<br/><br/><button></button>
+Shopify Development
+                    
+                    </h4>
+ 
+ </div>
+        </div>
+        </div>
+       </div>
+    
+
+    )
+
+}
+export default Web;
