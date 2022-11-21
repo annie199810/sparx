a5f18d6b777ff8848c3b7d2e746080be18493eb0
annie199810 <developerannie057@gmail.com>
2022-11-21 18:23:05 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 import Header from "./components/header/";
-
-
+import Hero from "./components/hero/hero";
+import Accla from "./components/acclamations/accla";
+import Motto from "./components/motto/motto";
+import Stake from "./components/stake/stake";
+import Web from "./components/web/web";
+import Block from "./components/block/block";
+import Class from "./components/reco/reco";
+import About from "./components/about/about";
 function App() {
   return (
     <div>
       <Header />
-      
+      <Hero />
+      <Accla />
+      <Motto />
+      <Stake />
+      <Web />
+      <Block />
+      <Class />
+      <About />
     </div>
   );
 }
